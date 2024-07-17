50035cf5f593f2d44bdcc8e808a15f75d38dc3e1
n1xsoph1c <pttrafiahmed@gmail.com>
2024-07-18 01:30:38 +0600

    Download Homepages of Gov sites

---
+++ ghost.py
@@ -0,0 +1,272 @@
+
+# Dictionary of websites to visit
+websites ={
+  "copyright.gov.bd": "http://copyrightoffice.gov.bd",
+  "ecs.gov.bd": "https://ecs.gov.bd",
+  "naogaon.gov.bd": "https://www.naogaon.gov.bd",
+  "nbr.gov.bd": "https://nbr.gov.bd",
+  "ldtax.gov.bd": "https://ldtax.gov.bd",
+  "apscl.gov.bd": "http://www.apscl.gov.bd",
+  "bangabhaban.gov.bd": "http://www.bangabhaban.gov.bd",
+  "bscic.gov.bd": "https://bscic.gov.bd/",
+  "bard.gov.bd": "https://bard.gov.bd/",
+  "ansarvdp.gov.bd": "https://ansarvdp.gov.bd/",
+  "baec.gov.bd": "https://baec.gov.bd/",
+  "banbeis.gov.bd": "https://banbeis.gov.bd/",
+  "bcic.gov.bd": "https://bcic.gov.bd/",
+  "bcsadminacademy.gov.bd": "https://bcsadminacademy.gov.bd/",
+  "berc.org.bd": "https://berc.org.bd",
+  "fdc.gov.bd": "http://www.fdc.gov.bd",
+  "bforest.gov.bd": "https://bforest.gov.bd/",
+  "dpp.gov.bd": "https://www.dpp.gov.bd/",
+  "bjri.gov.bd": "https://bjri.gov.bd/",
+  "blri.gov.bd": "https://blri.gov.bd/",
+  "bnswc.gov.bd": "https://bnswc.gov.bd/",
+  "petrobangla.org.bd": "http://www.petrobangla.org.bd",
+  "bof.gov.bd": "http://www.bof.gov.bd",
+  "police.gov.bd": "https://www.police.gov.bd/",
+  "bpdb.gov.bd": "https://bpdb.gov.bd/",
+  "edu.gov.bd": "https://www.edu.gov.bd",
+  "health.gov.bd": "https://www.health.gov.bd",
+  "lged.gov.bd": "https://mail.lged.gov.bd",
+  "moind.gov.bd": "http://www.moind.gov.bd",
+  "mosict.gov.bd": "http://www.mosict.gov.bd/",
+  "nsda.gov.bd": "https://nsda.gov.bd",
+  "rab.gov.bd": "https://www.rab.gov.bd/",
+  "railway.gov.bd": "http://www.railway.gov.bd/",
+  "reb.gov.bd": "http://www.reb.gov.bd",
+  "sreda.gov.bd": "http://www.sreda.gov.bd",
+  "bangladesh.gov.bd": "https://bangladesh.gov.bd/index.php",
+  "pmo.gov.bd": "http://www.pmo.gov.bd/",
+  "ssf.gov.bd": "http://www.ssf.gov.bd/",
+  "ngoab.gov.bd": "http://www.ngoab.gov.bd",
+  "cabinet.gov.bd": "http://www.cabinet.gov.bd",
+  "dcdhaka.gov.bd": "http://www.dcdhaka.gov.bd/",
+  "lgd.gov.bd": "http://www.lgd.gov.bd/",
+  "dphe.gov.bd": "http://www.dphe.gov.bd/",
+  "moestab.gov.bd": "http://www.moestab.gov.bd/",
+  "mof.gov.bd": "http://www.mof.gov.bd/",
+  "erd.gov.bd": "http://www.erd.gov.bd",
+  "barc.gov.bd": "http://www.barc.gov.bd/",
+  "mofdm.gov.bd": "http://www.mofdm.gov.bd/",
+  "dmb.gov.bd": "http://www.dmb.gov.bd/",
+  "mopt.gov.bd": "http://www.mopt.gov.bd/",
+  "bttb.gov.bd": "http://www.bttb.gov.bd/",
+  "mora.gov.bd": "http://www.mora.gov.bd",
+  "mos.gov.bd": "http://www.mos.gov.bd",
+  "plancomm.gov.bd": "http://www.plancomm.gov.bd/",
+  "bbs.gov.bd": "http://www.bbs.gov.bd/",
+  "moef.gov.bd": "http://www.moef.gov.bd",
+  "bfri.gov.bd": "http://www.bfri.gov.bd/",
+  "mod.gov.bd": "http://www.mod.gov.bd",
+  "bmd.gov.bd": "http://www.bmd.gov.bd/",
+  "motj.gov.bd": "http://www.motj.gov.bd",
+  "mohpw.gov.bd": "http://www.mohpw.gov.bd/",
+  "nha.gov.bd": "http://www.nha.gov.bd",
+  "mincom.gov.bd": "http://www.mincom.gov.bd",
+  "epb.gov.bd": "http://www.epb.gov.bd",
+  "powercell.gov.bd": "http://www.powercell.gov.bd",
+  "mocat.gov.bd": "http://www.mocat.gov.bd",
+  "mowca.gov.bd": "http://www.mowca.gov.bd",
+  "mofl.gov.bd": "http://www.mofl.gov.bd",
+  "moc.gov.bd": "http://www.moc.gov.bd/",
+  "rhd.gov.bd": "http://www.rhd.gov.bd/",
+  "brta.gov.bd": "http://www.brta.gov.bd/",
+  "brtc.gov.bd": "http://www.brtc.gov.bd/",
+  "dtcb.gov.bd": "http://www.dtcb.gov.bd/",
+  "moedu.gov.bd": "http://www.moedu.gov.bd/",
+  "dshe.gov.bd": "http://www.dshe.gov.bd/",
+  "educationboard.gov.bd": "http://www.educationboard.gov.bd/",
+  "mopme.gov.bd": "http://www.mopme.gov.bd",
+  "mowr.gov.bd": "http://www.mowr.gov.bd",
+  "moca.gov.bd": "http://www.moca.gov.bd",
+  "nanl.gov.bd": "http://www.nanl.gov.bd",
+  "mha.gov.bd": "http://www.mha.gov.bd",
+  "mlwa.gov.bd": "http://www.mlwa.gov.bd/",
+  "minlaw.gov.bd": "http://www.minlaw.gov.bd",
+  "eprocure.gov.bd": "http://www.eprocure.gov.bd/resources/common/StdTenderSearch.jsp?keyword=LGED&h=t&Itemid=204",
+  "bpsc.gov.bd": "https://bpsc.gov.bd/",
+  "siteprice.org": "http://www.siteprice.org/website-worth/bpdb.gov.bd",
+  "brri.gov.bd": "https://brri.gov.bd/",
+  "portal.gov.bd": "http://rhd.portal.gov.bd/",
+  "bsbk.gov.bd": "https://bsbk.gov.bd/",
+  "bteb.gov.bd": "https://bteb.gov.bd/",
+  "btrc.gov.bd": "http://www.btrc.gov.bd",
+  "btmc.gov.bd": "https://btmc.gov.bd/",
+  "bangladeshtradeportal.gov.bd": "https://www.bangladeshtradeportal.gov.bd",
+  "bwdb.gov.bd": "http://www.bwdb.gov.bd",
+  "bansdoc.gov.bd": "https://bansdoc.gov.bd/",
+  "bmda.gov.bd": "https://bmda.gov.bd/",
+  "biwta.gov.bd": "https://biwta.gov.bd/",
+  "cda.gov.bd": "https://cda.gov.bd/",
+  "cpa.gov.bd": "http://www.cpa.gov.bd",
+  "coastguard.gov.bd": "https://coastguard.gov.bd/",
+  "cdb.gov.bd": "https://cdb.gov.bd/",
+  "dae.gov.bd": "http://www.dae.gov.bd",
+  "dam.gov.bd": "https://dam.gov.bd/",
+  "coop.gov.bd": "http://www.coop.gov.bd",
+  "fisheries.gov.bd": "http://www.fisheries.gov.bd",
+  "dip.gov.bd": "http://www.dip.gov.bd",
+  "dss.gov.bd": "https://dss.gov.bd/",
+  "dot.gov.bd": "http://www.dot.gov.bd",
+  "dyd.gov.bd": "https://dyd.gov.bd/",
+  "dmp.gov.bd": "https://dmp.gov.bd/",
+  "dgdp.gov.bd": "http://dgdp.gov.bd/dgdp/AP_TEN/new/index.php",
+  "dia.gov.bd": "http://www.dia.gov.bd",
+  "techedu.gov.bd": "https://techedu.gov.bd/",
+  "emrd.gov.bd": "http://www.emrd.gov.bd",
+  "dgfp.gov.bd": "https://dgfp.gov.bd/",
+  "fri.gov.bd": "http://www.fri.gov.bd",
+  "imed.gov.bd": "http://www.imed.gov.bd",
+  "moi.gov.bd": "http://www.moi.gov.bd",
+  "educationboardresults.gov.bd": "http://www.educationboardresults.gov.bd/",
+  "ird.gov.bd": "http://www.ird.gov.bd",
+  "parliament.gov.bd": "https://www.parliament.gov.bd/",
+  "mochta.gov.bd": "http://www.mochta.gov.bd",
+  "probashi.gov.bd": "http://www.probashi.gov.bd",
+  "mohfw.gov.bd": "http://www.mohfw.gov.bd",
+  "mole.gov.bd": "http://www.mole.gov.bd",
+  "minland.gov.bd": "http://www.minland.gov.bd",
+  "msw.gov.bd": "http://www.msw.gov.bd",
+  "moysports.gov.bd": "http://www.moysports.gov.bd",
+  "nape.gov.bd": "http://www.nape.gov.bd",
+  "nctb.gov.bd": "http://www.nctb.gov.bd",
+  "nmst.gov.bd": "http://www.nmst.gov.bd",
+  "nationalsavings.gov.bd": "http://www.nationalsavings.gov.bd",
+  "powerdivision.gov.bd": "http://www.powerdivision.gov.bd",
+  "pwd.gov.bd": "https://pwd.gov.bd/",
+  "rdcd.gov.bd": "http://www.rdcd.gov.bd",
+  "sca.gov.bd": "http://www.sca.gov.bd",
+  "sob.gov.bd": "http://www.sob.gov.bd",
+  "naem.gov.bd": "http://www.naem.gov.bd",
+  "titasgas.gov.bd": "https://titasgas.gov.bd/",
+  "warpo.gov.bd": "http://www.warpo.gov.bd",
+  "moa.gov.bd": "http://www.moa.gov.bd/",
+  "bangladeshpost.gov.bd": "http://www.bangladeshpost.gov.bd/"
+}
+
+import os
+import subprocess
+import sys
+import time
+import random
+import logging
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
+
+# Function to download homepage using curl
+async def download_homepage(site_name, url, headers):
+    try:
+        logging.info(f"[!] Downloading {site_name}")
+        current_dir = os.getcwd()
+        file_path = os.path.join(current_dir, "websites", f"{site_name}.html")
+
+        # Build curl command
+        curl_command = [
+            "curl", "-o", f"{file_path}", "-L", "--retry", "3", "--retry-max-time", "5", "--insecure", url
+        ]
+        
+        # Add retry delay only if not on Windows
+        if not sys.platform.startswith('win'):
+            curl_command.extend(["--retry-delay", "0.5"])
+
+        # Execute curl command
+        process = subprocess.Popen(curl_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+
+        if process.returncode == 0:
+            logging.info(f"✔️ {site_name}")
+            return True
+        else:
+            logging.error(f"❌ {site_name}: {stderr.decode('utf-8').strip()}")
+            return False
+
+    except Exception as e:
+        logging.error(f"❌ {site_name}: {e}")
+        return False
+
+
+# Function to delete homepage
+async def delete_homepage(site_name):
+    try:
+        current_dir = os.getcwd()
+        file_path = os.path.join(current_dir, "websites", f"{site_name}.html")
+        os.remove(file_path)
+    except Exception as e:
+        logging.error(f"❌ {e}")
+
+# Function to handle site processing
+async def handle_site(site_name, url, headers):
+    asyncio.sleep(random.randint(1, 5))
+    try:
+        downloaded = await download_homepage(site_name, url, headers)
+        if downloaded:
+            await delete_homepage(site_name)
+        return downloaded
+    except Exception as e:
+        logging.error(f"❌ {e}")
+        return False
+
+# Function to run asyncio tasks
+def run_asyncio_tasks(site_name, url, headers):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        result = loop.run_until_complete(handle_site(site_name, url, headers))
+        return result
+    finally:
+        loop.close()
+
+# Main function to orchestrate tasks
+def main():
+    # Configure logging
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+        "Accept-Language": "en,en-US;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive"
+    }
+
+    cert_path = os.path.join(os.getcwd(), "selfsigned.crt")
+    key_path = os.path.join(os.getcwd(), "selfsigned.key")
+
+    num_threads = len(websites)
+    logging.info(f"[!] Using {num_threads} threads!\n")
+
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        try:
+            while True:
+                futures = []
+                successful_count = 0
+                failed_count = 0
+                
+                for site_name, url in websites.items():
+                    future = executor.submit(run_asyncio_tasks, site_name, url, headers)
+                    futures.append(future)
+
+                for future in futures:
+                    if future.result():
+                        successful_count += 1
+                    else:
+                        failed_count += 1
+
+                logging.info(f"\n\n[!] [{successful_count}/{successful_count + failed_count}] tasks done [ {failed_count} ] failed\n")
+
+                rand_sleep = random.randint(2, 10)
+                logging.info(f"[!] Restarting in {rand_sleep} seconds...\n")
+                time.sleep(rand_sleep)
+
+        except KeyboardInterrupt:
+            logging.info("\n[!] Exiting program...")
+            executor.shutdown(wait=False)
+            sys.exit(0)
+        except Exception as e:
+            logging.error(f"[❌] Unexpected error: {e}")
+            sys.exit(1)
+
+if __name__ == "__main__":
+    main()
